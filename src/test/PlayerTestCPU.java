6f8eee7ffe98a179cf2b84df070449e969def3ef
Desii99 <desireepasion.rodriguez@e-campus.uab.cat>
2020-11-14 14:31:00 +0100

    Added Board and BoatsStatus classes and PlayerTestCPU.java and PlayerTestHUM.java tests. Version 1.0 in Player.java and PlayerCPU.java classes.

---
+++ src/test/PlayerTestCPU.java
@@ -0,0 +1,17 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import game.PlayerCPU;
+
+/**
+ * 
+ * @author desirée
+ * @version 1.0
+ *
+ */
+class PlayerTestCPU {
+
+}
---
+++ src/test/PlayerTestHUM.java
@@ -0,0 +1,18 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import game.PlayerHUM;
+
+/**
+ * 
+ * @author desirée
+ * @version 1.0
+ *
+ */
+class PlayerTestHUM {
+
+
+}
