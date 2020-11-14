4963221496d5ae6ca6560f7209d264ed9acc15a1
Desii99 <desireepasion.rodriguez@e-campus.uab.cat>
2020-11-14 19:48:03 +0100

    Version 1.0 BoatsStatus.java

--- src/game/BoatsStatus.java
+++ src/game/BoatsStatus.java
@@ -1,9 +1,20 @@
 package game;
-
+import java.lang.String;
 /**
  * @autor desirée
  * @version 1.0
  */
-public class BoatsStatus{
-
+public final class BoatsStatus{
+  public static final String WATER_TYPE = "water";
+  public static final String CARRIER_TYPE = "carries";
+  public static final String BATTLESHIP_TYPE = "water";
+  public static final String CRUISER_TYPE = "cruiser";
+  public static final String SUBMARINE_TYPE = "submarine";
+  public static final String DESTROYER_TYPE = "destroyer";
+  public static final char WATER_CHAR = '^';
+  public static final char FOG_CHAR = '^';
+  public static final char BOAT_CHAR = '^';
+  private BoatsStatus(){
+    throw new AssertionError();
+  }
 }
