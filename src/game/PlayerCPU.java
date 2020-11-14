a4812a702630d49a2b76010b463a12d7e839a708
Desii99 <desireepasion.rodriguez@e-campus.uab.cat>
2020-11-14 19:01:15 +0100

    Version 1.3 PlayerCPU.java

--- src/game/PlayerCPU.java
+++ src/game/PlayerCPU.java
@@ -1,9 +1,14 @@
 package game;
 import java.util.*;
 import java.io.*;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyListener;
+import java.lang.String;
 /**
  * @author desirée
- * @version 1.2
+ * @version 1.3
  */
 //we should put Cursor cursor (we use posX, posY) and also Cell cell (we use cell) ??
 public class PlayerCPU implements Player{
@@ -17,6 +22,7 @@ public class PlayerCPU implements Player{
   private boolean aliveDestroyer;
   private boolean isWin;
   private Cursor cursor;
+  private String keyboard;
   public PlayerCPU(){
     this.boatsStatus = new BoatsStatus();
     this.cursor = new Cursor();
@@ -28,55 +34,87 @@ public class PlayerCPU implements Player{
     this.aliveCruiser = true;
     this.aliveSubmarine = true;
     this.aliveDestroyer = true;
+    this.keyboard = new String("");
   }
-
-  public void playTurn(int posX,int posY,int torn) throws Exception{
-    if(torn == 0) {
-      if (posX < 0) {
-        throw new Exception("posX or posY out of board.");
-      }
-      if (posY < 0) {
-        throw new Exception("posX or posY out of board.");
-      }
-      if (posX > 9) {
-        throw new Exception("posX or posY out of board.");
-      }
-      if (posY > 9) {
-        throw new Exception("posX or posY out of board.");
-      }
-      //Missing read arrows and enter.
-      //Loop where we look characters read.
-      //condition DOWN_KEY:
-
-      //within the condition:
-      if (posY > 0) {
-        cursor.setPosY(--posY);
-      }
-      //Condition UP_KEY:
-
-      //within the condition:
-      if (posY < 9) {
-        cursor.setPosY(++posY);
-      }
-
-      //Condition LEFT_KEY:
-
-      //within the condition:
-      if (posX > 0) {
-        cursor.setPosX(--posX);
-      }
-
-      //Condition RIGHT_KEY:
-
-      //within the condition:
+  @Override
+  public String keyReleased(int posX,int posY,KeyEvent e) {
+    // New key press
+    int key = e.getKeyCode();
+    // If we press right
+    if (key == KeyEvent.VK_RIGHT) {
       if (posX < 9) {
         cursor.setPosX(++posX);
       }
-      //Condition ENTER_KEY:
-
-      //within the condition:
-      //We put the calls to the functions later.
+    } else if (key == KeyEvent.VK_LEFT) {
+      if (posX > 0) {
+        cursor.setPosX(--posX);
+      }
+    }else if(key == KeyEvent.VK_DOWN){
+      if (posY > 0) {
+        cursor.setPosY(--posY);
+      }
+    }else if(key == KeyEvent.VK_UP){
+      if (posY < 9) {
+        cursor.setPosY(++posY);
+      }
     }
+    else if(key == KeyEvent.VK_ENTER){
+      //Change!!!!
+      if (posY < 9) {
+        cursor.setPosY(++posY);
+      }
+    }
+  }
+
+  @Override
+  public void keyTyped(int posX,int posY,KeyEvent e) {
+    // New key press
+    int key = e.getKeyCode();
+    // If we press right
+    if (key == KeyEvent.VK_RIGHT) {
+      if (posX < 9) {
+        cursor.setPosX(++posX);
+      }
+    } else if (key == KeyEvent.VK_LEFT) {
+      if (posX > 0) {
+        cursor.setPosX(--posX);
+      }
+    }else if(key == KeyEvent.VK_DOWN){
+      if (posY > 0) {
+        cursor.setPosY(--posY);
+      }
+    }else if(key == KeyEvent.VK_UP){
+      if (posY < 9) {
+        cursor.setPosY(++posY);
+      }
+    }else if(key == KeyEvent.VK_ENTER){
+      //Change!!!!
+      if (posY < 9) {
+        cursor.setPosY(++posY);
+      }
+    }
+  }
+  public void playTurn(int posX,int posY,int torn) throws Exception{
+
+    if (posX < 0) {
+      throw new Exception("posX or posY out of board.");
+    }
+    if (posY < 0) {
+      throw new Exception("posX or posY out of board.");
+    }
+    if (posX > 9) {
+      throw new Exception("posX or posY out of board.");
+    }
+    if (posY > 9) {
+      throw new Exception("posX or posY out of board.");
+    }
+    //Missing read arrows and enter.
+
+    //Condition ENTER_KEY:
+
+    //within the condition:
+    //We put the calls to the functions later.
+
   }
   private void updateBoardCell(int posX,int posY,Cell cell) throws Exception{
       if(posX < 0){
@@ -103,7 +141,7 @@ public class PlayerCPU implements Player{
     if(boatsCount == 5){
       win = true;
     }
-    return this.win;
+    return win;
   }
   public boolean isCarrierAlive(){
     return aliveCarrier;
